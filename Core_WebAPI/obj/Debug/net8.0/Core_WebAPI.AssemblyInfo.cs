d2bd2945f0df5f8c2f4f42cd125cad91ab867837
kondeboinaajay873 <ajaykondeboina873@gmail.com@example.com>
2026-01-20 11:33:17 +0530

    Initial Commit

--- Core_WebAPI/obj/Debug/net8.0/Core_WebAPI.AssemblyInfo.cs
+++ Core_WebAPI/obj/Debug/net8.0/Core_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac61e45cb88eaae6782f88a5fe702192db63e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
